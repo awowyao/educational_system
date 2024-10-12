26c39918cc807f625d8d500051d04aa3709b75d8
awowyao <wen_yao_1999@163.com>
2024-10-12 16:04:15 +0800

    更改后端项目

--- educationaladministrationsystem/src/main/java/com/cwy/educationaladministrationsystem/mapper/teacherMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cwy.educationaladministrationsystem.mapper;
-
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface teacherMapper {
-    Integer ByCourseIdClassIdGetTeacher(@Param("classId") Integer classId, @Param("courseId") Integer courseId);
-}
